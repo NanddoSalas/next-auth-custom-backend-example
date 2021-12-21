cfe82f51f9f5c714a35f89552cf048e6b1eb4b81
Fernando Salas <nanddosalas@gmail.com>
2021-12-20 17:01:23 -0700

    update Auth resolver
      * remove googleAuth mutation
      * add signin mutation

--- packages/server/src/Auth.resolver.ts
+++ packages/server/src/Auth.resolver.ts
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import {
   Arg,
   Ctx,
@@ -10,16 +11,19 @@ import {
 } from 'type-graphql';
 import { Context } from './types';
 import User from './User.entity';
-import { authenticateWithGoogle, createAccesToken } from './utils';
+import { createAccesToken } from './utils';
 
 @InputType()
-class GoogleAuthInput {
+class SignInInput {
   @Field()
-  idToken: string;
+  signInToken: string;
 }
 
 @ObjectType()
-class GoogleAuthPayload {
+class SignInPayload {
+  @Field(() => User, { nullable: true })
+  user?: User;
+
   @Field({ nullable: true })
   accesToken?: string;
 }
@@ -31,17 +35,67 @@ class AuthResolver {
     return user;
   }
 
-  @Mutation(() => GoogleAuthPayload)
-  async googleAuth(
-    @Arg('input') { idToken }: GoogleAuthInput,
-  ): Promise<GoogleAuthPayload> {
-    const user = await authenticateWithGoogle(idToken);
+  @Mutation(() => SignInPayload)
+  async signin(
+    @Arg('input')
+    { signInToken }: SignInInput,
+  ): Promise<SignInPayload> {
+    try {
+      const { provider, id, name, email, image } = jwt.verify(
+        signInToken,
+        process.env.SECRET || 'gCtvpTciwl/nPSvvWQrqn+kIXB7A/SpvRXX5CtfJNDI=',
+      ) as {
+        provider: string;
+        id: string;
+        name: string;
+        email: string;
+        image: string;
+      };
 
-    if (!user) return {};
+      const query = User.createQueryBuilder('user');
 
-    const accesToken = await createAccesToken(user);
+      if (provider === 'google') {
+        query.where('user.googleId = :id', { id });
+      }
 
-    return { accesToken };
+      if (provider === 'twitter') {
+        query.where('user.twitterId= :id', { id });
+      }
+
+      if (provider === 'github') {
+        query.where('user.githubId= :id', { id });
+      }
+
+      const user = await query.getOne();
+
+      if (user) {
+        const accesToken = await createAccesToken(user);
+
+        return { accesToken, user };
+      }
+
+      const emailAlreadyInUse = await User.findOne({ where: { email } });
+
+      if (emailAlreadyInUse) return {};
+
+      const newUser = User.create({ name, email, image });
+
+      if (provider === 'google') newUser.googleId = id;
+
+      if (provider === 'twitter') newUser.twitterId = id;
+
+      if (provider === 'github') newUser.githubId = id;
+
+      await newUser.save();
+
+      const accesToken = await createAccesToken(newUser);
+
+      return { accesToken, user: newUser };
+    } catch (error) {
+      console.log(error);
+    }
+
+    return {};
   }
 }
 
