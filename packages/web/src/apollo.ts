37e08e6255c0b648c98573407e82f969fccbb996
Fernando Salas <nanddosalas@gmail.com>
2021-12-20 18:33:37 -0700

    update apollo client

--- packages/web/src/apollo.ts
+++ packages/web/src/apollo.ts
@@ -1,6 +1,23 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { getSession } from 'next-auth/react';
+
+const httpLink = createHttpLink({
+  uri: process.env.SERVER_URL || 'http://localhost:4000/graphql',
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const { accesToken } = await getSession();
+
+  return {
+    headers: {
+      ...headers,
+      authorization: accesToken ? `Bearer ${accesToken}` : '',
+    },
+  };
+});
 
 export const client = new ApolloClient({
-  uri: process.env.SERVER_URL || 'http://localhost:4000/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
