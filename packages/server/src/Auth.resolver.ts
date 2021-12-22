05679001ddd00a8948f2a7a848e7394c6ebb7bb6
Fernando Salas <nanddosalas@gmail.com>
2021-12-22 12:00:13 -0700

    add unlinkProvider mutation

--- packages/server/src/Auth.resolver.ts
+++ packages/server/src/Auth.resolver.ts
@@ -1,4 +1,4 @@
-import { Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
 import { Context } from './types';
 import User from './User.entity';
 
@@ -6,6 +6,28 @@ import User from './User.entity';
 class AuthResolver {
   @Query(() => User, { nullable: true })
   me(@Ctx() { user }: Context) {
+    return user;
+  }
+
+  @Mutation(() => User, { nullable: true })
+  async unlinkProvider(
+    @Arg('provider') provider: string,
+    @Ctx() { user }: Context,
+  ): Promise<User | null> {
+    if (!user) return null;
+
+    const isUnlinkAllowed = user.connectedProviders().length > 1;
+
+    if (!isUnlinkAllowed) return user;
+
+    if (provider === 'google') user.googleId = '';
+    if (provider === 'twitter') user.twitterId = '';
+    if (provider === 'github') user.githubId = '';
+
+    try {
+      await user.save();
+    } catch (error) {}
+
     return user;
   }
 }
